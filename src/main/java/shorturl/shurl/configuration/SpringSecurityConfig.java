3497912ca1c8a75caba02018087ae6aca1f95157
Nickolai Vakulich <nickolai.vakulich@gmail.com>
2022-07-06 20:26:28 +0300

    pom update

--- src/main/java/shorturl/shurl/configuration/SpringSecurityConfig.java
+++ src/main/java/shorturl/shurl/configuration/SpringSecurityConfig.java
@@ -11,7 +11,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
         auth.inMemoryAuthentication()
                 .withUser("SomeUser").password("{noop}SomePassword").roles("USER");
     }
